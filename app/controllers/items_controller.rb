39cece473868eeeb7fd447aa1fb2e6959a49297b
hanari196 <ruru_8290xxchwt@yahoo.co.jp>
2025-08-31 20:41:30 +0900

    手順1_device導入_手順4_単体テストの実装

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,20 @@
+class ItemsController < ApplicationController
+  # index, show は誰でも見れる
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    # トップページ表示
+  end
+
+  def show
+    # 商品詳細表示
+  end
+
+  def new
+    # 出品ページ（ログイン必須）
+  end
+
+  def create
+    # 出品処理（ログイン必須）
+  end
+end
