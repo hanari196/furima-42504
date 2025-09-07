5767cba593900eb68795420f1d3bcfa328a5cd82
hanari196 <ruru_8290xxchwt@yahoo.co.jp>
2025-09-07 23:06:25 +0900

    手順3 ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_method
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_time
 end
