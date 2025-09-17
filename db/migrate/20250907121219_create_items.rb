151a6eb8ebcf30b68535e584eb63a16f48f9f3a7
hanari196 <ruru_8290xxchwt@yahoo.co.jp>
2025-09-17 20:08:43 +0900

    修正_1

--- db/migrate/20250907121219_create_items.rb
+++ db/migrate/20250907121219_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
-      t.string :title
-      t.text :product_contents
-      t.integer :category_id
-      t.integer :condition_id
-      t.integer :shipping_method_id
-      t.integer :prefecture_id
-      t.integer :delivery_time_id
-      t.integer :price
-      t.references :user, null: false, foreign_key: true
+      t.string  :title,              null: false
+      t.text    :product_contents,   null: false
+      t.integer :category_id,        null: false
+      t.integer :condition_id,       null: false
+      t.integer :shipping_method_id, null: false
+      t.integer :prefecture_id,      null: false
+      t.integer :delivery_time_id,   null: false
+      t.integer :price,              null: false
+      t.references :user,            null: false, foreign_key: true
 
       t.timestamps
     end
