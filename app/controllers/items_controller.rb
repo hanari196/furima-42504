68b14b25b6b6e5c15924d797e2346d27980816bb
hanari196 <ruru_8290xxchwt@yahoo.co.jp>
2025-09-21 10:54:31 +0900

    手順1 indexアクションへのルーティングを記述する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,44 @@
 class ItemsController < ApplicationController
   # index, show は誰でも見れる
-  # before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
+  # トップページ表示
   def index
-    # トップページ表示
+    @items = Item.all
   end
 
   def show
     # 商品詳細表示
   end
 
+  # 出品ページ（ログイン必須）
   def new
-    # 出品ページ（ログイン必須）
+    @item = Item.new
   end
 
+  # 出品処理（ログイン必須）
   def create
-    # 出品処理（ログイン必須）
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :title,
+      :product_contents,
+      :category_id,
+      :condition_id,
+      :shipping_method_id,
+      :prefecture_id,
+      :delivery_time_id,
+      :price, 
+      :image
+    ).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-
-   # ログイン済みユーザーのルート
-  authenticated :user do
-    root to: "items#index", as: :authenticated_root
-  end
-
-  # 未ログインユーザーのルート
-  unauthenticated do
-    root to: "items#index", as: :unauthenticated_root
-  end
-  
-  # 共通の root_path を作る
-  root to: "items#index"
-
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :items, only: [:index]
+  root "items#index"
 end
