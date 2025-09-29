b72e9074b65e52b7f318c2a4199ceb2bb6d1deaf
hanari196 <ruru_8290xxchwt@yahoo.co.jp>
2025-09-30 06:22:25 +0900

    手順2 決済機能を実装する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_address_params)
+
     if @order_address.valid?
       pay_item
       @order_address.save
@@ -21,18 +22,8 @@ class OrdersController < ApplicationController
 
 private
 
-  def order_address_params
-    params.permit(
-      :postal_code, 
-      :prefecture_id, 
-      :city, 
-      :house_number,
-      :building_name,
-      :phone_number
-    ).merge(user_id: current_user.id, item_id: @item.id)
-  end
-  
-  def set_item
+
+   def set_item
     @item = Item.find(params[:item_id])
   end
 
@@ -43,8 +34,20 @@ private
     end
   end
 
+    def order_address_params
+    params.require(:order_address).permit(
+      :postal_code, 
+      :prefecture_id, 
+      :city, 
+      :house_number,
+      :building_name,
+      :phone_number
+    ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+
     Payjp::Charge.create(
       amount: @item.price,
       card: order_address_params[:token],
