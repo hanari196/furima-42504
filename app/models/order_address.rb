221172b793014726b36bf181eae89776082ca211
hanari196 <ruru_8290xxchwt@yahoo.co.jp>
2025-10-08 19:35:14 +0900

    修正_with_optionsの使用

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -11,20 +11,17 @@ class OrderAddress
                 :token
 
   # バリデーション
-    validates :postal_code, presence: { message: "can't be blank" }
+  with_options presence: { message:"can't be blank" } do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'Input correctly' }
-
-    validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
-
-    validates :city, presence: { message: "can't be blank" }
-    validates :house_number, presence: { message: "can't be blank" }
-
-    validates :phone_number, presence: { message: "can't be blank" }
+    validates :city
+    validates :house_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
-
-    validates :token, presence: { message: "can't be blank" }
-    validates :user_id, presence: true
-    validates :item_id, presence: true
+    validates :token
+    validates :user_id
+    validates :item_id
+  end
+    
+    validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
 
   # 保存する処理
   def save
